1e4dcfb6b7607e52743fe70649f45e45c6223fe9
gguinea 🦫 <kamil5176@gmail.com>
2024-01-21 23:20:05 +0100

    Parsing first part - minutes

--- parser/parser.go
+++ parser/parser.go
@@ -1,16 +1,149 @@
 package parser
 
+import (
+	"errors"
+	"strconv"
+	"strings"
+)
+
 type Parser struct {
 	minutes []int
 	hours   []int
 	days    []int
 	months  []int
+	command string
 }
+
+// * * * * * Command_to_execute
 
 func NewParser() *Parser {
 	return &Parser{}
 }
 
-func (p *Parser) Parse(input string) string {
-	return ""
+func (p *Parser) Parse(input string) error {
+	inputTab, err := getSplitInput(input)
+	if err != nil {
+		return err
+	}
+
+	err = p.performParse(inputTab)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func getSplitInput(input string) ([]string, error) {
+	if len(input) == 0 {
+		return []string{}, errors.New("Input length should not be 0")
+	}
+
+	split := strings.Split(input, " ")
+
+	if len(split) != 6 {
+		return []string{}, errors.New("Input is in wrong format, should be: '* * * * * Command_to_execute'")
+	}
+	return split, nil
+}
+
+func (p *Parser) performParse(slicedInput []string) error {
+	res, err := parsePart(slicedInput[0])
+	p.minutes = res
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func parsePart(minutes string) ([]int, error) {
+	if strings.Contains(minutes, "/") {
+		split := strings.Split(minutes, "/")
+		multipleValues, err := generateValuesForRangeWithStep(split[0], split[1], 59)
+		if err != nil {
+			return []int{}, err
+		}
+		return multipleValues, nil
+	}
+
+	if strings.Contains(minutes, "-") {
+		split := strings.Split(minutes, "-")
+		multipleValues, err := generateValuesForRange(split[0], split[1])
+		if err != nil {
+			return []int{}, err
+		}
+		return multipleValues, nil
+	}
+
+	if strings.Contains(minutes, ",") {
+		split := strings.Split(minutes, ",")
+		multipleValues, err := parseMultipleIntsFromStringsSlice(split)
+		if err != nil {
+			return []int{}, err
+		}
+		return multipleValues, nil
+	}
+
+	res, err := strconv.Atoi(minutes)
+	if err != nil {
+		return []int{}, err
+	}
+	return []int{res}, nil
+}
+
+func parseMultipleIntsFromStringsSlice(inputToParse []string) ([]int, error) {
+	res := []int{}
+
+	for _, elem := range inputToParse {
+		parsed, err := strconv.Atoi(elem)
+		if err != nil {
+			return []int{}, err
+		}
+		res = append(res, parsed)
+	}
+
+	return res, nil
+}
+
+func generateValuesForRange(start string, stop string) ([]int, error) {
+	startParsed, err := strconv.Atoi(start)
+	if err != nil {
+		return []int{}, err
+	}
+
+	stopParsed, err := strconv.Atoi(stop)
+	if err != nil {
+		return []int{}, err
+	}
+
+	if startParsed > stopParsed {
+		return []int{}, errors.New("Wrong range values")
+	}
+
+	res := []int{}
+	for i := startParsed; i <= stopParsed; i++ {
+		res = append(res, i)
+	}
+
+	return res, nil
+}
+
+func generateValuesForRangeWithStep(start string, step string, maxValue int) ([]int, error) {
+	startParsed, err := strconv.Atoi(start)
+	if err != nil {
+		return []int{}, err
+	}
+
+	stepParsed, err := strconv.Atoi(step)
+	if err != nil {
+		return []int{}, err
+	}
+
+	res := []int{}
+	for i := startParsed; i <= maxValue; i += stepParsed {
+		res = append(res, i)
+	}
+
+	return res, nil
 }
--- parser/parser_test.go
+++ parser/parser_test.go
@@ -1,6 +1,7 @@
 package parser
 
 import (
+	"reflect"
 	"testing"
 )
 
@@ -8,5 +9,73 @@ func TestShouldReturnNewEmptyParserClient(t *testing.T) {
 	parser := NewParser()
 	if parser == nil {
 		t.Fatalf("Should return new parser")
+	}
+}
+
+func TestShouldReturnErrorWhenInputStringIsEmpty(t *testing.T) {
+	parser := NewParser()
+	err := parser.Parse("")
+	if err == nil {
+		t.Fatalf("Should return error when input is an empty string")
+	}
+}
+
+func TestShouldReturnErrorWhenInputDoesNotHaveSixParts(t *testing.T) {
+	parser := NewParser()
+	err := parser.Parse("1 1 1 111")
+	if err == nil {
+		t.Fatalf("Should return error when input has wrong format")
+	}
+}
+
+func TestShouldParseProperlySimpleDayInput(t *testing.T) {
+	parser := NewParser()
+	err := parser.Parse("1 1 1 1 1 1")
+
+	if err != nil {
+		t.Fatalf("Should not return error with proper input; %s", err)
+	}
+
+	if !reflect.DeepEqual(parser.minutes, []int{1}) {
+		t.Fatalf("Should parse minutes properly")
+	}
+}
+
+func TestShouldParseProperlyMultipleMinuteValuesSeparatedByCommas(t *testing.T) {
+	parser := NewParser()
+	err := parser.Parse("1,4,59 1 1 1 1 1")
+
+	if err != nil {
+		t.Fatalf("Should not return error with proper input; %s", err)
+	}
+
+	if !reflect.DeepEqual(parser.minutes, []int{1, 4, 59}) {
+		t.Fatalf("Should parse minutes properly")
+	}
+}
+
+func TestShouldParseProperlyMultipleMinuteValuesWithRangeOperatorInclusively(t *testing.T) {
+	parser := NewParser()
+	err := parser.Parse("1-4 1 1 1 1 1")
+
+	if err != nil {
+		t.Fatalf("Should not return error with proper input; %s", err)
+	}
+
+	if !reflect.DeepEqual(parser.minutes, []int{1, 2, 3, 4}) {
+		t.Fatalf("Should parse minutes properly")
+	}
+}
+
+func TestShouldParseProperlyMultipleMinuteValuesWithStepOperator(t *testing.T) {
+	parser := NewParser()
+	err := parser.Parse("1/10 1 1 1 1 1")
+
+	if err != nil {
+		t.Fatalf("Should not return error with proper input; %s", err)
+	}
+
+	if !reflect.DeepEqual(parser.minutes, []int{1, 11, 21, 31, 41, 51}) {
+		t.Fatalf("Should parse minutes properly, actual: %v", parser.minutes)
 	}
 }
